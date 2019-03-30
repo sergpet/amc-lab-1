c28532c861d57fa08de38b48e123368d9d91785a
sergey.petrov <sergey.petrov@kenshoo.com>
2019-03-30 08:14:43 +0100

    Initial Commit

---
+++ amc-lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  amc-lab
+//
+//  Created by Sergey Petrov on 2019-03-30.
+//  Copyright © 2019 Sergey Petrov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
